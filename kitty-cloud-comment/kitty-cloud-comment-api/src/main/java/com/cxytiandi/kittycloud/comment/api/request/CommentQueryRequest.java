b53b59462afd4ce6a59bcb15f0f28f40954b13b9
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-02-17 19:59:30 +0800

    评论服务jar依赖调整

--- kitty-cloud-comment/kitty-cloud-comment-api/src/main/java/com/cxytiandi/kittycloud/comment/api/request/CommentQueryRequest.java
+++ kitty-cloud-comment/kitty-cloud-comment-api/src/main/java/com/cxytiandi/kittycloud/comment/api/request/CommentQueryRequest.java
@@ -1,7 +1,9 @@
 package com.cxytiandi.kittycloud.comment.api.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -15,6 +17,8 @@ import java.io.Serializable;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentQueryRequest implements Serializable {
 
     /**
