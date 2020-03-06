b8052402500e42770a6ec112ece022f1a2a16e84
jihuan.yin@ipiaoniu.com <jihuan.yin@ipiaoniu.com>
2020-03-06 23:18:41 +0800

    Sleuth增加

---
+++ kitty-cloud-gateway/kitty-cloud-gateway-web/src/main/java/com/cxytiandi/kittycloud/gateway/web/filter/UserAuthFilter.java
@@ -0,0 +1,43 @@
+package com.cxytiandi.kittycloud.gateway.web.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户认证过滤器
+ *
+ * @作者 尹吉欢
+ * @个人微信 jihuan900
+ * @微信公众号 猿天地
+ * @GitHub https://github.com/yinjihuan
+ * @作者介绍 http://cxytiandi.com/about
+ * @时间 2020-03-06 23:11
+ */
+@Slf4j
+@Component
+public class UserAuthFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_ERROR_FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        log.info("请求进来了");
+        return null;
+    }
+}
